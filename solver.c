82a1e3711759e5008a378c6d39cd8082c9f0162d
Elouan MARTINET <emartine@e3r12p17.42.fr>
2017-11-17 18:58:39 +0100

    ok1

--- solver.c
+++ solver.c
@@ -36,13 +36,67 @@ static void	empty_square(char *square, size_t n)
 		square[i++] = '.';
 }
 
+#include <stdio.h>
+static void	copy_piece(struct s_solver_ctx *ctx, char letter, size_t square_pos)
+{
+	size_t	piece_start;
+	size_t	i;
+	size_t tmp = 0;
+
+	piece_start = (letter - 'A') * 16;
+	i = 0;
+	while (i < 16)
+	{
+		if (ctx->pieces[piece_start + i] == '#')
+		{
+			tmp++;
+			ctx->square[square_pos + i / 4 * ctx->square_side + i % 4] = letter;
+		}
+		i++;
+	}
+	//printf("===== copied %lu\n", tmp);
+	//fflush(stdout);
+}
+
 static int	place(struct s_solver_ctx *ctx, char letter)
 {
+	size_t	square_pos;
 	size_t	piece_id;
+	size_t	piece_pos;
 
-	(void)ctx;
+	square_pos = 0;
 	piece_id = letter - 'A';
-	return (42);
+	while (square_pos < ctx->square_side * ctx->square_side)
+	{
+		//printf("[begin piece %c on pos %lu]\n", letter, square_pos);
+		//fflush(stdout);
+		piece_pos = 0;
+		while (piece_pos < 16)
+		{
+			if (ctx->pieces[piece_id * 16 + piece_pos] == '#')
+			{
+				//printf("[sqside=%lu x=%lu y=%lu sqpos=%lu ppos=%lu]\n", ctx->square_side, square_pos % ctx->square_side + piece_pos % 4, square_pos / ctx->square_side + piece_pos / 4, square_pos, piece_pos);
+				//fflush(stdout);
+				if (square_pos % ctx->square_side + piece_pos % 4 >= ctx->square_side
+						|| square_pos / ctx->square_side + piece_pos / 4 >= ctx->square_side
+						|| ctx->square[square_pos + piece_pos / 4 *
+						ctx->square_side + piece_pos % 4 ] != '.')
+				{
+					break ;
+				}
+			}
+			piece_pos++;
+		}
+		if (piece_pos == 16)
+		{
+			//printf("------------ [copy piece %c on pos %lu]\n", letter, square_pos);
+			//fflush(stdout);
+			copy_piece(ctx, letter, square_pos);
+			return (1);
+		}
+	square_pos++;
+	}
+	return (0);
 }
 
 static int	comb_solv(struct s_solver_ctx *ctx)
@@ -50,6 +104,8 @@ static int	comb_solv(struct s_solver_ctx *ctx)
 	size_t	i;
 
 	empty_square(ctx->square, ctx->square_side * ctx->square_side);
+	//printf("------------------------- {%.*s} -----------------\n", (int)ctx->letter_id, ctx->letter_list);
+	//fflush(stdout);
 	i = 0;
 	while (i < ctx->letter_id)
 	{
@@ -60,7 +116,7 @@ static int	comb_solv(struct s_solver_ctx *ctx)
 	i = 0;
 	while (i < ctx->square_side)
 	{
-		write(1, ctx->square, ctx->square_side);
+		write(1, ctx->square + i * ctx->square_side, ctx->square_side);
 		write(1, "\n", 1);
 		i++;
 	}
@@ -92,11 +148,17 @@ static int	letter_comb(struct s_solver_ctx *ctx)
 int			solver(char *pieces, size_t n)
 {
 	struct s_solver_ctx	ctx;
+	size_t				i;
 
 	ctx.pieces = pieces;
 	ctx.pieces_length = n;
 	ctx.letter_id = 0;
-	ctx.square_side = 2; // TODO loop
-	letter_comb(&ctx);
+	i = 2;
+	while (1)
+	{
+		ctx.square_side = i++;
+		if(letter_comb(&ctx))
+			break ;
+	}
 	return (42);
 }
